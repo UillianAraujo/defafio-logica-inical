e8641308dea97b36910ed14e64ae60f8f2854af8
UillianAraujo <uillsilva52@gmail.com>
2025-03-17 21:19:57 -0300

    outroExemplo

--- aula-teste/index.js
+++ aula-teste/index.js
@@ -1,1 +1,5 @@
-console.log("Olá mundão");
+let nome = "Uillian Araujo"
+let idade = 30
+let profissao = "Desenvolvedor"
+
+console.log(`Meu nome é ${nome}, tenho ${idade} anos e sou ${profissao}.`)
